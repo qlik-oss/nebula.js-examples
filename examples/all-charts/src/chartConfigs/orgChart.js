6db922c0ad58534ec6e68f79a46f1d89f3677e0d
Li Kang <kkllpaul7766@gmail.com>
2022-08-31 12:15:24 +0200

    chore: bump dependencies in all charts and use esm core

--- examples/all-charts/src/chartConfigs/orgChart.js
+++ examples/all-charts/src/chartConfigs/orgChart.js
@@ -1,8 +1,8 @@
 export const orgChartConfig = {
   /** org chart config */
-  id: "JPYRPmJ",
   type: "sn-org-chart",
   element: document.querySelector(".org_root"),
+  fields: ["title", "id"],
   properties: {
     title: "[Example #09]: org chart",
   },
