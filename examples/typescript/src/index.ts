73a3c29a41b064d23f5f49a3212436a4a69830d5
caele <tsm@qlik.com>
2024-02-08 09:53:00 +0100

    chore: typescript example

---
+++ examples/typescript/src/index.ts
@@ -0,0 +1,25 @@
+import { useElement } from "@nebula.js/stardust";
+import properties from "./object-properties";
+import data from "./data";
+import ext from "./ext";
+/**
+ * Entrypoint for your sense visualization
+ * @param {object} galaxy Contains global settings from the environment.
+ * Useful for cases when stardust hooks are unavailable (ie: outside the component function)
+ * @param {object} galaxy.anything Extra environment dependent options
+ * @param {object=} galaxy.anything.sense Optional object only present within Sense,
+ * see: https://qlik.dev/extend/build-extension/in-qlik-sense
+ */
+export default function supernova(galaxy: any) {
+  return {
+    qae: {
+      properties,
+      data,
+    },
+    ext: ext(galaxy),
+    component() {
+      const element = useElement();
+      element.innerHTML = "<div>Hello!</div>"; // eslint-disable-line
+    },
+  };
+}
